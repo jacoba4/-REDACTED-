0d050ba845c0b8553d90cf2cf48022adf690656b
Austin <jacoba4@rpi.edu>
2019-02-20 17:12:37 -0500

    Final Puzzle Framework

---
+++ New Unity Project/Assets/Scripts/FinalPuzzle.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalPuzzle : MonoBehaviour
+{
+
+    public bool moving;
+    public int currsymbol;
+    int finalcount;
+    GameObject[] faces;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        finalcount = 5;
+        moving = false;
+        currsymbol = 1;
+        faces = GameObject.FindGameObjectsWithTag("FinalPuzzleFace");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!moving)
+        {
+            if(Input.GetKeyDown(KeyCode.A))
+                {
+                    StartCoroutine("Rot",60f);
+                    SetMoving(true);
+                }
+                else if(Input.GetKeyDown(KeyCode.D))
+                {
+                    StartCoroutine("Rot",-60f);
+                    SetMoving(true);
+                }
+        }
+    }
+
+
+    void SetMoving(bool x)
+    {
+        moving = x;
+    }
+
+    IEnumerator Rot(float degrees)
+    {
+        float t = 0;
+        float degrees_rotated = 0;
+        float target = transform.eulerAngles.y + degrees;
+       while(Mathf.Abs(degrees_rotated) <= 60)
+       {
+           float lerp = 0;
+           if(degrees > 0)
+           {
+                lerp = Mathf.Lerp(2,0,t);
+           }
+
+           else if(degrees < 0)
+           {
+               lerp = -Mathf.Lerp(2,0,t);
+           }
+           transform.Rotate(0,lerp,0,Space.World);
+           degrees_rotated += lerp;
+           t+=.1f*Time.deltaTime;
+           yield return null;
+       }
+       transform.eulerAngles = new Vector3(transform.eulerAngles.x,target,transform.eulerAngles.z);
+       SetMoving(false);
+        
+    }
+
+    void Check(int x)
+    {
+        if(x == currsymbol)
+        {
+            currsymbol++;
+            if(currsymbol == finalcount)
+            {
+                print("SOLVED :D");
+            }
+        }
+        else
+        {
+            Reset();
+        }
+    }
+
+    void Reset()
+    {
+        currsymbol = 1;
+        foreach(GameObject face in faces)
+        {
+            face.GetComponent<SpriteRenderer>().color = Color.red;
+        }
+    }
+
+}
---
+++ New Unity Project/Assets/Scripts/FinalPuzzleFace.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalPuzzleFace : MonoBehaviour
+{
+    public int order;
+    public GameObject puzzle;
+    FinalPuzzle script;
+    // Start is called before the first frame update
+    void Start()
+    {
+        puzzle = GameObject.FindGameObjectWithTag("FinalPuzzle");
+        script = puzzle.GetComponent<FinalPuzzle>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.green;
+        script.SendMessage("Check",order);
+       
+    }
+}
