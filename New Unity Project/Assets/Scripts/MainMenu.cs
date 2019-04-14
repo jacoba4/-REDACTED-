3a203e29f34f9e8a0ef45d6d96b226b7fca10215
Unknown <laura.mastracchio18@gmail.com>
2019-04-14 14:19:44 -0400

    Main menu added, also added to build settings so it works

---
+++ New Unity Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Image controlImage;
+    public Sprite controlScreen;
+
+    public Image titleScreenSpace;
+    public Sprite titleScreen;
+
+    public Sprite transparent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        titleScreenSpace.sprite = titleScreen;
+        controlImage.sprite = transparent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            controlImage.sprite = transparent;
+        }
+    }
+    void ChangeScene ()
+    {
+        SceneManager.LoadScene("Alpha", LoadSceneMode.Single);
+    }
+    void ShowControls()
+    {
+        controlImage.sprite = controlScreen;
+    }
+}
