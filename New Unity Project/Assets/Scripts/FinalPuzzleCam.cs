d17705dc3bd023604494c2bb963002799b77b032
Austin <jacoba4@rpi.edu>
2019-04-15 22:30:25 -0400

    More working on music

--- New Unity Project/Assets/Scripts/FinalPuzzleCam.cs
+++ New Unity Project/Assets/Scripts/FinalPuzzleCam.cs
@@ -7,16 +7,19 @@ public class FinalPuzzleCam : MonoBehaviour
     public GameObject crosshair;
     bool play;
     public AudioClip[] musicclips;
+    float timeelapsed;
     // Start is called before the first frame update
     void Start()
     {
         transform.GetComponent<Camera>().enabled = false;
         play = false;
+        timeelapsed = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timeelapsed += Time.deltaTime;
         if(play)
         {
             Music();
@@ -43,6 +46,6 @@ public class FinalPuzzleCam : MonoBehaviour
 
     void Music()
     {
-
+        
     }
 }
